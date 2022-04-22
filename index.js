cc1a5591d8183f605b1dc572c9c9d10485f6915f
ming wu <a85062@pchome.com.tw>
2022-04-22 23:45:58 +0800

    【修改】加上swagger網頁的連結在註解裡

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use('/',postRouter);
 // app.use(express.json());
 // urlencoded是用來解析非網址列傳入的資料
 app.use(express.urlencoded({ extended: true }));
+// 路徑 https://week3-express.herokuapp.com/api-doc/ 沒在postman檔案裡
 app.use('/api-doc',swaggerUi.serve,swaggerUi.setup(swaggerSetting));
 const port =process.env.PORT || 3000;
 app.listen(port,()=>{
